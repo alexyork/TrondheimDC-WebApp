430989f1d5eec9c9733e1fc48155ce21ebbfb790
Alex York <alex-york@hotmail.co.uk>
2012-06-28 08:31:21 +0200

    Unit tests and code for return the talks in alphabetical order

--- src/examples/plain-js/tests/TrondheimDC.Talks.Tests.js
+++ src/examples/plain-js/tests/TrondheimDC.Talks.Tests.js
@@ -14,6 +14,23 @@ describe("TrondheimDC.Speakers", function() {
         expect(talks.length).toEqual(2);
     });
     
+    it("should get talks in alphabetical order", function() {
+        // Arrange
+        setupTalks([
+            { speaker: "Alex York", title: "JavaScript rocks" },
+            { speaker: "Andre Tangen", title: "CSS3 rocks" },
+            { speaker: "Pål Strøm", title: "HTML5 rocks" },
+        ]);
+        
+        // Act
+        var talks = TrondheimDC.Talks.getAll();
+        
+        // Assert
+        expect(talks[0].title).toEqual("CSS3 rocks");
+        expect(talks[1].title).toEqual("HTML5 rocks");
+        expect(talks[2].title).toEqual("JavaScript rocks");
+    });
+    
     function setupTalks(talks) {
         window.bootstrapData = { talks: talks };
     }
