e5225babbb61fa9cd5f0a7769c47dfd505935cee
Alex York <alex-york@hotmail.co.uk>
2012-06-30 21:47:10 +0200

    Put the main.js stuff into an enclosed self-executing function to bring its variables out of the global namespace

--- js/app/main.js
+++ js/app/main.js
@@ -1,17 +1,21 @@
-var sessions = new TrondheimDC.Collections.SessionsList();
-sessions.reset([
-    {
-        title: "Maybe Backbone.js rocks, afterall...",
-        speaker: "Alex York",
-        tags: [ "backbone", "javascript", "lolcats" ]
-    },
-    {
-        title: "See! I told you Backbone.js rocks!",
-        speaker: "Andre Tangen",
-        tags: [ "backbone", "javascript", "bill-murray" ]
-    }
-]);
+(function() {
 
-var sessionsListView = new TrondheimDC.Views.SessionsListView({ collection: sessions });
-sessionsListView.render();
-$('#app-content').append(sessionsListView.el);
+    var sessions = new TrondheimDC.Collections.SessionsList();
+    sessions.reset([
+        {
+            title: "Maybe Backbone.js rocks, afterall...",
+            speaker: "Alex York",
+            tags: [ "backbone", "javascript", "lolcats" ]
+        },
+        {
+            title: "See! I told you Backbone.js rocks!",
+            speaker: "Andre Tangen",
+            tags: [ "backbone", "javascript", "bill-murray" ]
+        }
+    ]);
+    
+    var sessionsListView = new TrondheimDC.Views.SessionsListView({ collection: sessions });
+    sessionsListView.render();
+    $('#app-content').append(sessionsListView.el);
+
+})();
