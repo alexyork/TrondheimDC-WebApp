90c6e92769e986fcde55d3912dc7e217e15a997c
Alex York <alex-york@hotmail.co.uk>
2012-06-30 23:42:09 +0200

    Added Jasmine for unit testing our models and collections

---
+++ js/tests/collections/SessionsListTests.js
@@ -0,0 +1,12 @@
+describe("SessionsList", function() {
+    
+    describe("search", function() {
+        
+        it("should find sessions with matching tags", function() {
+            // TODO
+            expect(true).toEqual(false);
+        });
+        
+    });
+    
+});
