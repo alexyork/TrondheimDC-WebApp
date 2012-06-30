cd3c0ee3a5d9e448a337fcc5661147e1aa5081b0
Alex York <alex-york@hotmail.co.uk>
2012-07-01 00:20:32 +0200

    Got filter by tag working, and unit tested

--- src/js/tests/collections/SessionsListTests.js
+++ src/js/tests/collections/SessionsListTests.js
@@ -3,8 +3,16 @@ describe("SessionsList", function() {
     describe("search", function() {
         
         it("should find sessions with matching tags", function() {
-            // TODO
-            expect(true).toEqual(false);
+            var sessionsList = new TrondheimDC.Collections.SessionsList();
+            sessionsList.reset([
+                { title: "Jasmine is awesome", tags: [ "tdd", "unit-testing" ] },
+                { title: "JavaScript is awesome", tags: [ "javascript" ] }
+            ]);
+            
+            var searchResults = sessionsList.search("tdd");
+            
+            expect(searchResults.length).toEqual(1);
+            expect(searchResults.models[0].get("title")).toEqual("Jasmine is awesome");
         });
         
     });
