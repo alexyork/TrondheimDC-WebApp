b9225b815e36296d0c8cf065d21f753469cfaf8b
Alex York <alex-york@hotmail.co.uk>
2012-07-01 00:29:24 +0200

    A refactoring where the logic for determining if a session contains a tag, is moved to the session model, from the sessionlist collection.

--- js/app/models/Session.js
+++ js/app/models/Session.js
@@ -7,8 +7,9 @@ if (typeof TrondheimDC.Models === "undefined" || !TrondheimDC.Models) {
 
 TrondheimDC.Models.Session = Backbone.Model.extend({
     
-    containsTag: function() {
-        console.log("containsTag");
+    containsTag: function(tag) {
+        var tags = this.get("tags");
+        return _.indexOf(tags, tag) > -1;
     }
     
 });
--- js/tests/models/SessionTests.js
+++ js/tests/models/SessionTests.js
@@ -1,3 +1,29 @@
 describe("Session", function() {
     
+    describe("containsTag", function() {
+       
+       it("should report that it contains a tag, if that tag exists", function() {
+          var session = new TrondheimDC.Models.Session({
+              title: "Backbone.js kicks @$$",
+              tags: [ "backbone", "javascript" ]
+          });
+          
+          var result = session.containsTag("backbone");
+          
+          expect(result).toEqual(true);
+       });
+       
+       it("should report that it does not contain a tag, if that tag does not exist", function() {
+          var session = new TrondheimDC.Models.Session({
+              title: "Backbone.js kicks @$$",
+              tags: [ "backbone", "javascript" ]
+          });
+          
+          var result = session.containsTag("foobar");
+          
+          expect(result).toEqual(false);
+       });
+        
+    });
+    
 });
