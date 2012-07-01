47dd7b4ee07553c9cac56b917eead81bb22f01d7
andtan <jornandretangen@gmail.com>
2012-07-01 22:30:23 +0200

    done some router work, need lightweight server to do that. Using nodejs for now

---
+++ frontend/js/app/views/AppView.js
@@ -0,0 +1,23 @@
+if (typeof TrondheimDC === "undefined" || !TrondheimDC) {
+    TrondheimDC = {};
+}
+if (typeof TrondheimDC.Views === "undefined" || !TrondheimDC.Views) {
+    TrondheimDC.Views = {};
+}
+
+TrondheimDC.Views.AppView = Backbone.View.extend({
+    
+    el: $("body"),
+
+    setContentView: function(view) {
+      if(this.contentView) {
+        this.contentView.unbind()
+        this.contentView.remove()
+      }
+      if(view) {
+        this.contentView = view
+        this.$el.find("#app-content").html( $(this.contentView.el) ) 
+      }
+    }
+    
+});
