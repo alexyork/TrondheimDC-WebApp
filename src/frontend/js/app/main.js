b80741e569e002ba0996312a6e632422e3425c16
andtan <jornandretangen@gmail.com>
2012-07-01 22:30:23 +0200

    done some router work, need lightweight server to do that. Using nodejs for now

---
+++ src/frontend/js/app/main.js
@@ -0,0 +1,40 @@
+(function() {
+    
+    window.app = Object.create(Backbone.Events);
+    
+    var appView = new TrondheimDC.Views.AppView()
+
+    var router = new TrondheimDC.Routers.AppRouter({
+      delegateView: appView
+    })
+
+    router.route('sessions', 'sessions', function() {
+        // Get the sessions data into a list
+        var sessionsList = new TrondheimDC.Collections.SessionsList();
+        var sessionData = TrondheimDC.getAllSessions();
+        sessionsList.reset(sessionData);
+        
+        // Render the list into a view, and add to the DOM
+        var sessionsListView = new TrondheimDC.Views.SessionsListView({ collection: sessionsList });
+        sessionsListView.render();
+        appView.setContentView(sessionsListView)
+    })
+
+    router.route('speakers', 'speakers', function() {
+        //!TODO: render speaksers
+        appView.setContentView( /* speakers */)
+    })
+
+    router.route('favourites', 'favourites', function() {
+        //!TODO: render favourites
+        appView.setContentView( /* favourites */)
+    })
+
+    router.route('tweets', 'tweets', function() {
+        //!TODO: render tweets
+        appView.setContentView( /* tweets */)
+    })
+
+    Backbone.history.start()
+
+})();
---
+++ src/frontend/js/app/routers/AppRouter.js
@@ -0,0 +1,35 @@
+if (typeof TrondheimDC === "undefined" || !TrondheimDC) {
+    TrondheimDC = {};
+}
+if (typeof TrondheimDC.Routers === "undefined" || !TrondheimDC.Routers) {
+    TrondheimDC.Routers = {};
+}
+
+TrondheimDC.Routers.AppRouter = Backbone.Router.extend({
+    
+    initialize: function( options ) {
+        options.delegateView ? this.setupDelegateView( options.delegateView ) : void 0
+    },
+
+    setupDelegateView: function(view) {
+        view.$el.delegate('a', 'click.router', _.bind(this.clickListener, this))
+    },
+
+    sameOrigin: function(location, test) {
+        var port, origin
+        port = location.port && location.port != '80' ? ':' + location.port : ''
+        origin = location.protocol + '//' + location.hostname + port
+        return test.match( origin ) ? true : false
+    },
+
+    clickListener: function(event) {
+        var url, path
+        url = event.currentTarget.href
+        if( !this.sameOrigin(window.location, url) )
+          return null
+        event.preventDefault()
+        path = event.currentTarget.pathname + event.currentTarget.search
+        this.navigate(path, {trigger: true})
+    }
+    
+});
