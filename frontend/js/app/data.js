9679f82cc88272fb527ed8a82c3762bf711e0db1
Alex York <alex-york@hotmail.co.uk>
2012-07-03 21:10:19 +0200

    Changed the data into arrays, not functions. I dont want new copies each time it is called

--- frontend/js/app/data.js
+++ frontend/js/app/data.js
@@ -2,40 +2,36 @@ if (typeof TrondheimDC === "undefined" || !TrondheimDC) {
     TrondheimDC = {};
 }
 
-TrondheimDC.getAllSessions = function() {
-    return [
-        {   
-            id: 1,
-            title: "Maybe Backbone.js rocks, afterall...",
-            speakerId: 1,
-            tags: [ "backbone", "javascript", "lolcats" ]
-        },
-        {
-            id: 2,
-            title: "See! I told you Backbone.js rocks!",
-            speakerId: 2,
-            tags: [ "backbone", "javascript", "bill-murray" ]
-        },
-        {
-            id: 3,
-            title: "The art of changing your mind",
-            speakerId: 1,
-            tags: [ "no-backbone", "mindset", "lol" ]
-        },
-    ];
-};
+TrondheimDC.sessionsBootstrapData = [
+    {   
+        id: 1,
+        title: "Maybe Backbone.js rocks, afterall...",
+        speakerId: 1,
+        tags: [ "backbone", "javascript", "lolcats" ]
+    },
+    {
+        id: 2,
+        title: "See! I told you Backbone.js rocks!",
+        speakerId: 2,
+        tags: [ "backbone", "javascript", "bill-murray" ]
+    },
+    {
+        id: 3,
+        title: "The art of changing your mind",
+        speakerId: 1,
+        tags: [ "no-backbone", "mindset", "lol" ]
+    },
+];
 
-TrondheimDC.getAllSpeakers = function() {
-    return [
-        {
-            id: 1,
-            name: "Alex York",
-            description: "One happy little Englishman."
-        },
-        {
-            id: 2,
-            name: "Andre Tangen",
-            description: "Bill Murray..?"
-        }
-    ]
-};
+TrondheimDC.speakersBootstrapData = [
+    {
+        id: 1,
+        name: "Alex York",
+        description: "One happy little Englishman."
+    },
+    {
+        id: 2,
+        name: "Andre Tangen",
+        description: "Bill Murray..?"
+    }
+];
