b5aa42bd133689676146cd5d4b7bf5493b5e5a50
Pål Strøm <peirix@gmail.com>
2012-07-03 20:49:43 +0200

    cleaned up the sessionsListTest

--- frontend/js-tests/app/collections/SpeakersListTests.js
+++ frontend/js-tests/app/collections/SpeakersListTests.js
@@ -1,14 +1,7 @@
 describe("SpeakersList", function() {
     
     describe("getById", function() {
-        
         it("should find speaker with matching id", function() {
-            var sessionsList = new TrondheimDC.Collections.SessionsList();
-            sessionsList.reset([
-                { title: "Jasmine is awesome", tags: [ "tdd", "unit-testing" ] },
-                { title: "JavaScript is awesome", tags: [ "javascript" ] }
-            ]);
-
             var speakersList = new TrondheimDC.Collections.SpeakersList();
             speakersList.reset([
                 { id: 1, name: "Paulini" }
