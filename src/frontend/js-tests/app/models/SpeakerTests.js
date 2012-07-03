71b71cc90ed8d5739e82afe445c77a47da7f4793
Alex York <alex-york@hotmail.co.uk>
2012-07-03 22:50:36 +0200

    Refactored one of Påls tests. I think that we should have a separate test for making sure that upper and lower case searches should also work.

--- src/frontend/js-tests/app/models/SpeakerTests.js
+++ src/frontend/js-tests/app/models/SpeakerTests.js
@@ -3,23 +3,27 @@ describe("Speaker", function() {
     describe("matchesName", function() {
     
         it("should match search terms if it is found in the speaker name", function() {
-            var speaker = new TrondheimDC.Models.Speaker({
-                name: "Gøran"
-            });
+            var speaker = new TrondheimDC.Models.Speaker({ name: "Joe Bloggs" });
             
-            var result = speaker.matchesName("gøran");
+            var result = speaker.matchesName("Joe");
             
             expect(result).toEqual(true);
         });
         
         it("should not match search terms if it is not found in the speaker name", function() {
-            var speaker = new TrondheimDC.Models.Speaker({
-                name: "Alex"
-            });
+            var speaker = new TrondheimDC.Models.Speaker({ name: "Joe Bloggs" });
             
-            var result = speaker.matchesName("foobar");
+            var result = speaker.matchesName("Doe");
             
             expect(result).toEqual(false);
+        });
+        
+        it("should match search terms regardless of casing", function() {
+            var speaker = new TrondheimDC.Models.Speaker({ name: "Joe Bloggs" });
+            
+            var result = speaker.matchesName("bLOggS");
+            
+            expect(result).toEqual(true);
         });
     
     });
