8fbca9452a5344b59d7a1016b4e8ad381966b636
Pål Strøm <peirix@gmail.com>
2012-07-03 22:02:10 +0200

    Implemented search on speakerId in session w/tests

--- src/frontend/js/app/models/Session.js
+++ src/frontend/js/app/models/Session.js
@@ -14,6 +14,14 @@ TrondheimDC.Models.Session = Backbone.Model.extend({
     matchesSpeaker: function(searchTerm) {
     	return this.speaker.get("name").toLowerCase().indexOf(searchTerm.toLowerCase()) > -1;
     },
+
+    isPresentedBy: function(speakerId) {
+        for (var i = 0; i < this.speakers.length; i++) {
+            if (this.speakers[i].id == speakerId)
+                return true;
+        }
+        return false;
+    },
     
     containsTag: function(tag) {
         var tags = this.get("tags");
