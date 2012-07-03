f3f2df20e5fd13acc535b9fefe7a178b25a0814a
Pål Strøm <peirix@gmail.com>
2012-07-03 23:12:39 +0200

    Listing out all sessions by a speaker in the detail view

--- src/frontend/js/app/models/Session.js
+++ src/frontend/js/app/models/Session.js
@@ -16,9 +16,14 @@ TrondheimDC.Models.Session = Backbone.Model.extend({
     },
 
     isPresentedBy: function(speakerId) {
+<<<<<<< HEAD
         var speakers = this.get("speakers");
         for (var i = 0; i < speakers.length; i++) {
             if (speakers[i].id == speakerId)
+=======
+        for (var i = 0; i < this.get("speakers").length; i++) {
+            if (this.get("speakers")[i].id == speakerId)
+>>>>>>> Listing out all sessions by a speaker in the detail view
                 return true;
         }
         return false;
