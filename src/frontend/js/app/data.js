e75688609e444014de2c7a936a595eeb34cdac3d
andtan <jornandretangen@gmail.com>
2012-07-06 00:38:42 +0200

    Added empty Date objects for starts and ends for all sessions

--- src/frontend/js/app/data.js
+++ src/frontend/js/app/data.js
@@ -10,19 +10,25 @@ TrondheimDC.Data.sessions = [
         id: 1,
         title: "Maybe Backbone.js rocks, afterall...",
         speakerIds: [1],
-        tags: [ "backbone", "javascript", "lolcats" ]
+        tags: [ "backbone", "javascript", "lolcats" ],
+        starts: new Date(),
+        ends: new Date()
     },
     {
         id: 2,
         title: "See! I told you Backbone.js rocks!",
         speakerIds: [2],
         tags: [ "backbone", "javascript", "bill-murray" ]
+        starts: new Date(),
+        ends: new Date()
     },
     {
         id: 3,
         title: "The art of changing your mind",
         speakerIds: [1, 2],
         tags: [ "no-backbone", "mindset", "lol" ]
+        starts: new Date(),
+        ends: new Date()
     },
 ];
 
