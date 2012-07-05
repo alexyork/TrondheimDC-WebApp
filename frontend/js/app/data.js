0a2b4bd2c60c13f1e541bb4642c349b690ef7233
andtan <jornandretangen@gmail.com>
2012-07-06 01:36:14 +0200

    Added data for starts and end times

--- frontend/js/app/data.js
+++ frontend/js/app/data.js
@@ -11,24 +11,24 @@ TrondheimDC.Data.sessions = [
         title: "Maybe Backbone.js rocks, afterall...",
         speakerIds: [1],
         tags: [ "backbone", "javascript", "lolcats" ],
-        starts: new Date(),
-        ends: new Date()
+        starts: new Date(2012, 09, 01, 10, 00, 00),
+        ends: new Date(2012, 09, 01, 10, 45, 00)
     },
     {
         id: 2,
         title: "See! I told you Backbone.js rocks!",
         speakerIds: [2],
         tags: [ "backbone", "javascript", "bill-murray" ],
-        starts: new Date(),
-        ends: new Date()
+        starts: new Date(2012, 09, 01, 10, 15, 00),
+        ends: new Date(2012, 09, 01, 11, 00, 00)
     },
     {
         id: 3,
         title: "The art of changing your mind",
         speakerIds: [1, 2],
         tags: [ "no-backbone", "mindset", "lol" ],
-        starts: new Date(),
-        ends: new Date()
+        starts: new Date(2012, 09, 01, 12, 00, 00),
+        ends: new Date(2012, 09, 01, 12, 45, 00)
     },
 ];
 
