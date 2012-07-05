72197feed23558dcfa35c7c820c64baf169e66d4
andtan <jornandretangen@gmail.com>
2012-07-05 23:02:57 +0200

    Added methods: favorite, unfavourite and isFavourited. And static property localStorageFavouritesKey

--- frontend/js/app/models/Session.js
+++ frontend/js/app/models/Session.js
@@ -7,6 +7,13 @@ if (typeof TrondheimDC.Models === "undefined" || !TrondheimDC.Models) {
 
 TrondheimDC.Models.Session = Backbone.Model.extend({
 
+    initialize: function() {
+        if(this.isFavourited())
+            this.set('favourited', true)
+        else
+            this.set('favourited', false)
+    },
+
     matchesTitle: function(searchTerm) {
         return this.get("title").toLowerCase().indexOf(searchTerm.toLowerCase()) > -1;
     },
@@ -33,6 +40,49 @@ TrondheimDC.Models.Session = Backbone.Model.extend({
             }
         })
         return matchFound;
+    },
+
+    isFavourited: function() {
+        var currentFavourites
+        currentFavourites = JSON.parse( localStorage.getItem(TrondheimDC.Models.Session.localStorageFavouritesKey) )
+        if(!currentFavourites || !currentFavourites.length)
+            return false
+        return ( currentFavourites.indexOf(this.get('id')) !== -1 )
+    },
+
+    favourite: function() {
+        var currentFavourites
+        try {
+            currentFavourites = JSON.parse( localStorage.getItem(TrondheimDC.Models.Session.localStorageFavouritesKey) )
+            if(!currentFavourites)
+                currentFavourites = []
+            if(!this.isFavourited())
+                currentFavourites.push(this.get('id'))
+            localStorage.setItem( TrondheimDC.Models.Session.localStorageFavouritesKey, JSON.stringify(currentFavourites) )
+            this.set('favourited', true)
+        } catch (e) {
+            this.trigger('error', e)
+        }
+    },
+
+    unfavourite: function() {
+        var currentFavourites, idIndex
+        try {
+            currentFavourites = JSON.parse( localStorage.getItem(TrondheimDC.Models.Session.localStorageFavouritesKey) )
+            if(currentFavourites) {
+                idIndex = currentFavourites.indexOf(this.get('id'))
+                if(idIndex !== -1)
+                    currentFavourites.splice(idIndex, 1)
+                localStorage.setItem( TrondheimDC.Models.Session.localStorageFavouritesKey, JSON.stringify(currentFavourites) )
+                this.set('favourited', false)
+            }   
+        } catch(e) {
+            this.trigger('error', e)
+        }
     }
     
+},
+{
+    //Statics
+    localStorageFavouritesKey: "favouriteSessions"
 });
