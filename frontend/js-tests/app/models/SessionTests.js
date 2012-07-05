2748316aeff4c710115b086e63e1d610ddaca772
andtan <jornandretangen@gmail.com>
2012-07-06 00:37:30 +0200

    tests for method timeslotConflictingWith

--- frontend/js-tests/app/models/SessionTests.js
+++ frontend/js-tests/app/models/SessionTests.js
@@ -1,5 +1,14 @@
 describe("Session", function() {
     
+    var times = {
+        "10:00:00" : new Date(2012, 01, 09, 10, 00, 00),
+        "10:45:00" : new Date(2012, 01, 09, 10, 45, 00),
+        "10:15:00" : new Date(2012, 01, 09, 10, 15, 00),
+        "11:00:00" : new Date(2012, 01, 09, 11, 00, 00),
+        "13:00:00" : new Date(2012, 01, 09, 13, 00, 00),
+        "13:45:00" : new Date(2012, 01, 09, 13, 45, 00),
+    }
+
     describe("containsTag", function() {
        
        it("should report that it contains a tag, if that tag exists", function() {
@@ -99,77 +108,129 @@ describe("Session", function() {
 
     describe("favourites", function() {
       
-      beforeEach(function() {
-        localStorage.clear()
-      })
-
-      describe("favourite", function() {
-        
-        it("should save the session id to and array of favourite session ids in localStorage  on key TrondheimDC.Models.Session.localStorageFavouritesKey", function() {
-          var sessionA = new TrondheimDC.Models.Session({ id: 123 })
-          var sessionB = new TrondheimDC.Models.Session({ id: 124 })
-
-          sessionA.favourite()
-          sessionB.favourite()
-
-          var favouriteIds = JSON.parse(localStorage.getItem(TrondheimDC.Models.Session.localStorageFavouritesKey))
-
-          expect(favouriteIds.length).toEqual(2)
-          expect(favouriteIds).toContain(123)
-          expect(favouriteIds).toContain(124)
+        beforeEach(function() {
+            localStorage.clear()
         })
 
-        it("should not create duplicate id in array if allready favourtied", function() {
-          var sessionA = new TrondheimDC.Models.Session({ id: 122 })
+        describe("favourite", function() {
+          
+          it("should save the session id to and array of favourite session ids in localStorage  on key TrondheimDC.Models.Session.localStorageFavouritesKey", function() {
+              var sessionA = new TrondheimDC.Models.Session({ id: 123 })
+              var sessionB = new TrondheimDC.Models.Session({ id: 124 })
 
-          sessionA.favourite()
-          sessionA.favourite()
+              sessionA.favourite()
+              sessionB.favourite()
 
-          var favouriteIds = JSON.parse(localStorage.getItem(TrondheimDC.Models.Session.localStorageFavouritesKey))
+              var favouriteIds = JSON.parse(localStorage.getItem(TrondheimDC.Models.Session.localStorageFavouritesKey))
 
-          expect(favouriteIds.length).toEqual(1)
-          expect(favouriteIds).toContain(122)
-        })
+              expect(favouriteIds.length).toEqual(2)
+              expect(favouriteIds).toContain(123)
+              expect(favouriteIds).toContain(124)
+          })
+
+          it("should not create duplicate id in array if allready favourtied", function() {
+              var sessionA = new TrondheimDC.Models.Session({ id: 122 })
+
+              sessionA.favourite()
+              sessionA.favourite()
+
+              var favouriteIds = JSON.parse(localStorage.getItem(TrondheimDC.Models.Session.localStorageFavouritesKey))
+
+              expect(favouriteIds.length).toEqual(1)
+              expect(favouriteIds).toContain(122)
+          })
 
       })
 
       describe("unfavourite", function() {
 
-        var sessionA
-        var sessionB
+          var sessionA
+          var sessionB
 
-        beforeEach(function() {
-          localStorage.clear()
+          beforeEach(function() {
+              localStorage.clear()
 
-          sessionA = new TrondheimDC.Models.Session({ id: 123 })
-          sessionB = new TrondheimDC.Models.Session({ id: 124 })
-          
-          sessionA.favourite()
-          sessionB.favourite()
-          
-          var favouriteIds = JSON.parse(localStorage.getItem(TrondheimDC.Models.Session.localStorageFavouritesKey))
-          expect(favouriteIds.length).toEqual(2)
-          expect(favouriteIds).toContain(123)
-          expect(favouriteIds).toContain(124)
-        })
+              sessionA = new TrondheimDC.Models.Session({ id: 123 })
+              sessionB = new TrondheimDC.Models.Session({ id: 124 })
+              
+              sessionA.favourite()
+              sessionB.favourite()
+              
+              var favouriteIds = JSON.parse(localStorage.getItem(TrondheimDC.Models.Session.localStorageFavouritesKey))
+              expect(favouriteIds.length).toEqual(2)
+              expect(favouriteIds).toContain(123)
+              expect(favouriteIds).toContain(124)
+          })
 
-        it("should delete the sessions id from the array stored in localStorage on key TrondheimDC.Models.Session.localStorageFavouritesKey", function() {
-          
-          sessionA.unfavourite()
+          it("should delete the sessions id from the array stored in localStorage on key TrondheimDC.Models.Session.localStorageFavouritesKey", function() {
+              
+              sessionA.unfavourite()
 
-          var favouriteIds = JSON.parse(localStorage.getItem(TrondheimDC.Models.Session.localStorageFavouritesKey))
-          expect(favouriteIds.length).toEqual(1)
-          expect(favouriteIds).toNotContain(123)
-          expect(favouriteIds).toContain(124)
+              var favouriteIds = JSON.parse(localStorage.getItem(TrondheimDC.Models.Session.localStorageFavouritesKey))
+              expect(favouriteIds.length).toEqual(1)
+              expect(favouriteIds).toNotContain(123)
+              expect(favouriteIds).toContain(124)
 
-          sessionB.unfavourite()
+              sessionB.unfavourite()
 
-          var favouriteIds = JSON.parse(localStorage.getItem(TrondheimDC.Models.Session.localStorageFavouritesKey))
-          expect(favouriteIds.length).toEqual(0)
+              var favouriteIds = JSON.parse(localStorage.getItem(TrondheimDC.Models.Session.localStorageFavouritesKey))
+              expect(favouriteIds.length).toEqual(0)
 
-        })
+          })
 
       })
+
+    })
+
+    describe("timeslotConflictingWith", function() {
+        
+        it("should return true if the session parameter has a start time within the timeslot of another favourited session", function() {
+
+            var favouritedSession = new TrondheimDC.Models.Session({
+                starts: times["10:00:00"],
+                ends: times["10:45:00"]
+            })
+            var conflictingSession = new TrondheimDC.Models.Session({
+                starts: times["10:15:00"],
+                ends: times["11:00:00"]
+            })
+            var passing = new TrondheimDC.Models.Session({
+                starts: times["13:00:00"],
+                ends: times["13:45:00"]
+            })
+
+            expect(
+                favouritedSession.timeslotConflictsWith(conflictingSession)
+            ).toEqual(true)
+            expect(
+                favouritedSession.timeslotConflictsWith(passing)
+            ).toEqual(false)
+
+        })
+
+        it("should return true if the session parameter has a end time within the timeslot of another favourited session", function() {
+
+            var favouritedSession = new TrondheimDC.Models.Session({
+                starts: times["10:15:00"],
+                ends: times["11:00:00"]
+            })
+            var conflictingSession = new TrondheimDC.Models.Session({
+                starts: times["10:00:00"],
+                ends: times["10:45:00"]
+            })
+            var passing = new TrondheimDC.Models.Session({
+                starts: times["13:00:00"],
+                ends: times["13:45:00"]
+            })
+
+            expect(
+                favouritedSession.timeslotConflictsWith(conflictingSession)
+            ).toEqual(true)
+            expect(
+                favouritedSession.timeslotConflictsWith(passing)
+            ).toEqual(false)
+
+        })
 
     })
     
