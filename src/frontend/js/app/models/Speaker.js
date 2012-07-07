b5538f23ba2a8bf5182834065cd09f992206fe85
andtan <jornandretangen@gmail.com>
2012-07-07 14:22:55 +0200

    formatting

--- src/frontend/js/app/models/Speaker.js
+++ src/frontend/js/app/models/Speaker.js
@@ -8,7 +8,7 @@ if (typeof TrondheimDC.Models === "undefined" || !TrondheimDC.Models) {
 TrondheimDC.Models.Speaker = Backbone.Model.extend({
 
     matchesName: function(searchTerm) {
-    	return this.get("name").toLowerCase().indexOf(searchTerm.toLowerCase()) > -1;
+        return this.get("name").toLowerCase().indexOf(searchTerm.toLowerCase()) > -1;
     }
     
 });
--- src/frontend/js/app/views/AppView.js
+++ src/frontend/js/app/views/AppView.js
@@ -10,14 +10,14 @@ TrondheimDC.Views.AppView = Backbone.View.extend({
     el: $("body"),
 
     setContentView: function(view) {
-      if(this.contentView) {
-        this.contentView.unbind()
-        this.contentView.remove()
-      }
-      if(view) {
-        this.contentView = view
-        this.$el.find("#app-content").html( $(this.contentView.el) ) 
-      }
+        if(this.contentView) {
+            this.contentView.unbind();
+            this.contentView.remove();
+        }
+        if(view) {
+            this.contentView = view;
+            this.$el.find("#app-content").html( $(this.contentView.el) );
+        }
     }
     
 });
