8bce138d65084d6842efa55c0a70cbc33e10e00e
Pål Strøm <peirix@gmail.com>
2012-07-15 16:16:35 +0200

    Added links to tweets
    Missing tests for regex, will do this soon.

--- src/frontend/js/app/views/TwitterView.js
+++ src/frontend/js/app/views/TwitterView.js
@@ -15,9 +15,29 @@ TrondheimDC.Views.TwitterView = Backbone.View.extend({
     },
     
     render: function() {
+        this.addLinks();
         var tweetHtml = this.template(this.model);
         this.$el.html(tweetHtml);
         return this;
+    },
+
+    addLinks: function() {
+        //TODO: Test these regexes
+        var text = this.model.text;
+        //replace links
+        var regex = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gi;
+        var replacement = '<a href="$1" rel="nofollow" title="$1">$1</a>';
+        text = text.replace(regex, replacement);
+        //replace handles
+        regex = /@([a-zA-Z0-9_]*)/gi;
+        replacement = '<a href="http://twitter.com/$1" title="$1 profile on Twitter" rel="nofollow">@$1</a>';
+        text = text.replace(regex, replacement);
+        //replace hashtags
+        regex = /\#([a-zA-Z0-9_]*)/gi;
+        replacement = '<a href="http://search.twitter.com/search?q=%23$1" title="search for $1 on Twitter" rel="nofollow">\#$1</a>';
+        text = text.replace(regex, replacement);
+
+        this.model.text = text;
     }
     
 });
