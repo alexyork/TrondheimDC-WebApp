ab92ceb85833d3c7bacb248669d846f5d800f2fe
Alex York <alex-york@hotmail.co.uk>
2012-09-15 14:03:32 +0200

    changed the way we point to the frontendfolder

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -4,7 +4,8 @@ var fs = require('fs');
  * GET home page.
  */
 exports.index = function(req, res) {
-    fs.readFile(__dirname + '/../../frontend/app.html', 'utf8', function(err, html) {
+    //fs.readFile(__dirname + '/../../frontend/app.html', 'utf8', function(err, html) {
+    fs.readFile('./frontend/app.html', 'utf8', function(err, html) {
         if (err) {
             return res.send(err)
         }
