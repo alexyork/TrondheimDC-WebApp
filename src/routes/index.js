b280a0f0edf3f6c5c02a1112a2f3b439f9ae94c5
Alex York <alex-york@hotmail.co.uk>
2012-09-15 14:48:36 +0200

    Major restructureing and cleanups. Had to remove the server folder, and removed all refs to jade, etc

---
+++ src/routes/index.js
@@ -0,0 +1,13 @@
+var fs = require('fs');
+
+/*
+ * GET home page.
+ */
+exports.index = function(req, res) {
+    fs.readFile(__dirname + '/../frontend/app.html', 'utf8', function(err, html) {
+        if (err) {
+            return res.send(err)
+        }
+        return res.send(html);
+    });
+};
