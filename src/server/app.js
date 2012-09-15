fd1ba1919244ec9f1df753a3a23ca0759b605b45
Alex York <alex-york@hotmail.co.uk>
2012-09-15 13:59:54 +0200

    various changes to try and debug a heroku problem

--- src/server/app.js
+++ src/server/app.js
@@ -1,30 +1,29 @@
-
-/**
+/*
  * Module dependencies.
  */
-
-var express = require('express')
-  , routes = require('./routes')
+var express = require('express');
+var routes = require('./routes');
 
 var app = module.exports = express.createServer();
 
-// Configuration
-
+/*
+ * Configuration
+ */
 app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/../frontend'));
+    //app.set('views', __dirname + '/views');
+    //app.set('view engine', 'jade');
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(__dirname + '/../frontend'));
 });
 
 app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
 });
 
 app.configure('production', function(){
-  app.use(express.errorHandler()); 
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
 // Routes
