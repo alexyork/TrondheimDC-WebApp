102c56509cefbc34e975104c1dabb01c4d0b8be7
Alex York <alex-york@hotmail.co.uk>
2012-09-15 14:14:07 +0200

    Trying to debug heroku

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,6 +5,19 @@ var fs = require('fs');
  */
 exports.index = function(req, res) {
     console.log("dirname: " + __dirname);
+    
+    fs.readdir(__dirname + "/../", function(err, files) {
+        for (var i = 0; i < files.length; i++) {
+            console.log("One level up: " + files[i]);
+        }
+    });
+    
+    fs.readdir(__dirname + "/../../", function(err, files) {
+        for (var i = 0; i < files.length; i++) {
+            console.log("Two levels up: " + files[i]);
+        }
+    });
+    
     fs.readFile(__dirname + '/../../frontend/app.html', 'utf8', function(err, html) {
         if (err) {
             return res.send(err)
