26acd71fd027b21afd8f81fde358715f392e6d80
andtan <jornandretangen@gmail.com>
2012-09-18 11:14:06 +0200

    moved procfile so we can deply this repo to heroku directly

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,15 @@ var fs = require('fs');
 /*
  * GET home page.
  */
-exports.index = function(req, res) {
-    fs.readFile(__dirname + '/../frontend/app.html', 'utf8', function(err, html) {
-        if (err) {
-            return res.send(err)
-        }
-        return res.send(html);
-    });
-};
+exports.index = function( req, res ) {
+
+    var htmlStream = fs.createReadStream( __dirname + '/../frontend/app.html' )
+
+    htmlStream.on( 'error', function( err ) {
+      console.log( 'error', err )
+      res.send( 'internal server error', 500 )
+    })
+    
+    htmlStream.pipe( res )
+
+}
