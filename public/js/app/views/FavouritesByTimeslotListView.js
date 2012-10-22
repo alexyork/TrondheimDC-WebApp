2f21315e1e5a38bb951a72de378efd7c91ec263a
Alex York <alex-york@hotmail.co.uk>
2012-10-22 20:09:48 +0200

    Favoutites now does not show the search section.

--- public/js/app/views/FavouritesByTimeslotListView.js
+++ public/js/app/views/FavouritesByTimeslotListView.js
@@ -7,6 +7,8 @@ if (typeof TrondheimDC.Views === 'undefined' || !TrondheimDC.Views) {
 
 TrondheimDC.Views.FavouritesByTimeslotListView = TrondheimDC.Views.SessionsByTimeslotListView.extend({
     
+    template: _.template( document.getElementById('favourites-by-timeslot-list-template').innerHTML ),
+    
     events: _.extend({  }, TrondheimDC.Views.SessionsByTimeslotListView.prototype.events),
     
     ListViewClass: TrondheimDC.Views.FavouritesListView,
